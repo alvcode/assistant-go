b4c952d0d2fd5d793c78daeae8e2e7cbfea668ce
kowashi <kitovnikov@gmail.com>
2025-03-09 13:44:42 +0300

    закреп заметок

--- migrations/20250309104142_alter_notes_table_add_pinned_column.sql
+++ migrations/20250309104142_alter_notes_table_add_pinned_column.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE notes ADD COLUMN pinned BOOLEAN;
+ALTER TABLE notes ADD COLUMN pinned BOOLEAN NOT NULL DEFAULT (false);
 -- +goose StatementEnd
 
 -- +goose Down
